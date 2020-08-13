d64009b2acbd1d384237700aa2dbaa7becc6a96d
nurtilex <nurtilex@gmail.com>
2020-08-13 20:13:45 +0600

    changed color palette, added tools and history panels. Changed some styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,50 @@
-const list = document.querySelector(".list");
+//todolist
+const list = document.querySelector('.list');
 const items = list.children;
-const emptyListMessage = document.querySelector(".no-tasks-message");
-const addBtn = document.querySelector(".add-btn");
-const input = document.querySelector(".input");
+const emptyListMessage = document.querySelector('.no-tasks-message');
+const addBtn = document.querySelector('.add-btn');
+const input = document.querySelector('.input');
 
-// deletes clicked item from the html collection
+
+//tools
+const deleteAllBtn = document.querySelector('.delete-all');
+
+//history
 const removeOnEvent = function (collection, btnSelector, event) {
-  Array.from(collection).forEach((item) => {
-    const btn = item.querySelector(btnSelector);
-    btn.addEventListener(event, function () {
-      item.remove();
-      items.length < 1 && emptyListMessage.classList.remove("hidden");
-      items.length <= 8 && list.classList.remove("vert-scroll");
-    });
-  });
+	Array.from(collection).forEach((item) => {
+		const btn = item.querySelector(btnSelector);
+		if (btnSelector === '.checkbox') {
+			btn.addEventListener(event, function () {
+				item.classList.add('btn-active');
+				setTimeout(function() {
+					item.remove();
+					items.length < 1 && emptyListMessage.classList.remove('hidden');
+					items.length <= 9 && list.classList.remove('vert-scroll');
+				}, 700);
+			});
+		} else {
+			btn.addEventListener(event, function () {
+				item.remove();
+				items.length < 1 && emptyListMessage.classList.remove('hidden');
+				items.length <= 9 && list.classList.remove('vert-scroll');
+			});
+		}
+	});
 };
 
-// adding new element to the list
-addBtn.addEventListener("click", function () {
-  const taskTemplate = document.querySelector("#task-template").content;
-  const newItemTemplate = taskTemplate.querySelector(".list-item");
-  const task = newItemTemplate.cloneNode(true);
-  const taskDescription = task.querySelector("span");
-  if (input.value.length >= 3) {
-    taskDescription.textContent = input.value;
-    list.appendChild(task);
-    removeOnEvent([task], ".checkbox", "change");
-    removeOnEvent([task], ".delete-btn", "click");
-    emptyListMessage.classList.add("hidden");
-    items.length > 8 && list.classList.add("vert-scroll");
-    input.value = "";
-  }
+
+addBtn.addEventListener('click', function () {
+	const taskTemplate = document.querySelector('#task-template').content;
+	const newItemTemplate = taskTemplate.querySelector('.list-item');
+	const task = newItemTemplate.cloneNode(true);
+	const taskDescription = task.querySelector('span');
+	if (input.value.length >= 3) {
+		taskDescription.textContent = input.value;
+		list.appendChild(task);
+		removeOnEvent([task], '.checkbox', 'change');
+		removeOnEvent([task], '.delete-btn', 'click');
+		emptyListMessage.classList.add('hidden');
+		items.length > 9 && list.classList.add('vert-scroll');
+		input.value = '';
+	}
 });
